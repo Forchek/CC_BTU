9a499d2774760c5b4235a9d71d04a7462e04b8b3
unknown <alvaropachecomartinez@gmail.com>
2023-01-29 09:56:17 +0100

    Sort in layer Working

--- Assets/Scripts/Camera/Mov_BTU.cs
+++ Assets/Scripts/Camera/Mov_BTU.cs
@@ -7,12 +7,14 @@ public class Mov_BTU : MonoBehaviour
     private int Speed = 6;
     private Vector2 Mov = new Vector2(0, 0);
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
 
     public Transform CameraPos;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -24,6 +26,7 @@ public class Mov_BTU : MonoBehaviour
     private void FixedUpdate()
     {
         Moving();
+        ChangeRenderOrder();
     }
 
     private void GetInputs()
@@ -45,4 +48,26 @@ public class Mov_BTU : MonoBehaviour
     {
         rb.velocity = Mov.normalized * Speed;
     }
+
+    private void ChangeRenderOrder()
+    {
+        if (transform.position.y >= CameraPos.position.y)
+        {
+            sr.sortingOrder = 1;
+        }
+        else if ((transform.position.y >= CameraPos.position.y -2) && (transform.position.y < CameraPos.position.y))
+        {
+            sr.sortingOrder = 2;
+        }
+        else
+        {
+            sr.sortingOrder = 3;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("CHOCANDO");
+    }
+
 }
